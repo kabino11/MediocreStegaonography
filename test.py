217051d047f2d0772cb9677175ddd02b3b7bc868
kabino11 <kabino11@gmail.com>
2025-05-27 22:58:05 -0600

    Update test.py
    Changes test file to use more generic file names

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 import steg
 
-size = os.stat("Academic Transcript.pdf").st_size * 8
+size = os.stat("test file.pdf").st_size * 8
 
 remain = size % 3
 pix_req = size / 3
@@ -11,7 +11,7 @@ pix_req = size / 3
 if remain > 0:
 	pix_req += 1
 
-img = Image.open("true1080x8.jpg")
+img = Image.open("test image.jpg")
 img = img.convert('RGB')
 
 print(pix_req, "pixels needed")
@@ -27,7 +27,7 @@ else:
 
 exit(0)
 
-with open("Academic Transcript.pdf", "rb") as f:
+with open("test file.pdf", "rb") as f:
 	byte = f.read(1)
 	while byte != "":
 		print(byte)
